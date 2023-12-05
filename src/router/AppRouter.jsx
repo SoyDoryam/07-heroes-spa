eec8166b18b560c4a055f8c739f6b3aa40df153e
Doryam <rizo0655@gmail.com>
2023-12-05 17:56:58 -0600

    Colocar clase de la ruta activa

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,28 +1,28 @@
-// Importa los componentes necesarios de react-router-dom
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 
-// Importa los componentes de las páginas necesarios
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DCPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+import { MarvelPage, DcPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
 
-// Define el componente principal del router de la aplicación
 export const AppRouter = () => {
   return (
     <>
-        {/* Utiliza el componente 'Routes' para definir las rutas */}
+        {/* Navbar es importado de "../ui" y se coloca al inicio de la aplicación */}
+        <Navbar />
+
+        {/* Definición de las rutas usando el componente Routes */}
         <Routes>
-            {/* Ruta para la página de Marvel, renderiza el componente 'MarvelPage' */}
-            <Route path="marvel" element={<MarvelPage />}/>
-            
-            {/* Ruta para la página de DC, renderiza el componente 'DcPage' */}
-            <Route path="dc" element={<DcPage/>}/>
-            
-            {/* Ruta para la página de login, renderiza el componente 'LoginPage' */}
-            <Route path="login" element={<LoginPage/>}/>
-            
-            {/* Ruta por defecto, redirige a la página de Marvel */}
-            <Route path="/" element={<Navigate to="/marvel"/>}/>
+            {/* Ruta para la página de Marvel */}
+            <Route path="marvel" element={<MarvelPage />} />
+
+            {/* Ruta para la página de DC */}
+            <Route path="dc" element={<DcPage />} />
+
+            {/* Ruta para la página de Login */}
+            <Route path="login" element={<LoginPage />} />
+
+            {/* Ruta por defecto que redirige a la página de Marvel */}
+            <Route path="/" element={<Navigate to="/marvel" />} />
         </Routes>
     </>
   )
