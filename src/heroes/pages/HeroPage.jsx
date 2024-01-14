c8206e3e94983d0124e4d5e3e73a4493569db5c8
Doryam <rizo0655@gmail.com>
2024-01-14 15:45:10 -0600

    Estilo del componente HeroScreen

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,20 +1,61 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
   // Obtiene los parámetros de la URL, en este caso, el 'id' del héroe.
   const { id } = useParams();
 
+  // Utiliza la función useNavigate de React Router para manejar la navegación.
+  const navigate = useNavigate();
+
   // Utiliza la función getHeroById para obtener la información del héroe según su 'id'.
   const hero = getHeroById(id);
+
+  // Función que maneja la navegación para regresar a la página anterior.
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
 
   // Si no se encuentra información del héroe, redirige al usuario a la página de Marvel.
   if (!hero) {
     return <Navigate to="/marvel" />;
   }
 
-  // Si se encuentra información del héroe, muestra su nombre en un elemento <h1>.
+  // Si se encuentra información del héroe, muestra la página del héroe.
   return (
-    <h1>{hero.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        {/* Muestra la imagen del héroe con un formato de miniatura */}
+        <img 
+          src={ `/assets/heroes/${id}.jpg` }
+          alt={hero.superhero} 
+          className="img-thumbnail"
+        />
+      </div>
+
+      <div className="col-8">
+        {/* Muestra el nombre del héroe en un elemento <h3> */}
+        <h3>{hero.superhero}</h3>
+
+        {/* Muestra información detallada del héroe en una lista */}
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b> <p>{hero.alter_ego}</p></li>
+          <li className="list-group-item"><b>Editor/Publisher:</b> <p>{hero.publisher}</p></li>
+          <li className="list-group-item"><b>Primera aparición:</b> <p>{hero.first_appearance}</p></li>
+
+          {/* Muestra los personajes asociados al héroe */}
+          <h5 className="mt-3">Personajes:</h5>
+          <p>{hero.characters}</p>
+
+          {/* Botón para regresar a la página anterior */}
+          <button 
+            className="btn btn-outline-primary" 
+            onClick={onNavigateBack}
+          >
+            Regresar
+          </button>
+        </ul>
+      </div>
+    </div>
   );
 };
