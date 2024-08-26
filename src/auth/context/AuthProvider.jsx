012449a756ed5c4bdcc9f5cc7ae9fef8de47c8bf
Doryam <rizo0655@gmail.com>
2024-08-26 15:23:40 -0600

    autn agregando cambios

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,26 +1,34 @@
 import { useReducer } from 'react';
-import { AuthContext } from './AuthContext';
-import { AuthReducer } from './AuthReducer';
-import { types } from '../types/types';
+import { AuthContext } from './AuthContext'; // Importamos el contexto de autenticación
+import { AuthReducer } from './AuthReducer'; // Importamos el reducer de autenticación
+import { types } from '../types/types'; // Importamos los tipos de acciones
 
-const initialState = {
-    logged: false,
+
+// Función de inicialización para obtener el estado inicial del contexto de autenticación
+const init = () => {
+    const user = JSON.parse( localStorage.getItem('user') ); // Obtenemos el usuario almacenado en localStorage
+
+    return {
+        logged: !!user, // Si hay un usuario en localStorage, establecemos logged como true
+        user, // Establecemos el usuario obtenido de localStorage
+    }
 }
 
 // Crea el proveedor que utiliza el contexto de autenticación
 export const AuthProvider = ({ children }) => {
-    const [authState, dispatch] = useReducer(AuthReducer, initialState);
+    const [authState, dispatch] = useReducer(AuthReducer, {}, init); // Usamos useReducer con el reducer de autenticación y la función de inicialización
 
     // Función para iniciar sesión
     const login = (name = '') => {
+
+        const user = {id: 'ABC', name}
         // Crea una acción de inicio de sesión con un payload que incluye un identificador y el nombre del usuario
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user
         }     
+
+        localStorage.setItem('user', JSON.stringify(user))
 
         // Despacha la acción
         dispatch(action);
@@ -29,10 +37,10 @@ export const AuthProvider = ({ children }) => {
     // Retorna el proveedor de contexto de autenticación con el estado de autenticación y la función de inicio de sesión
     return (
         <AuthContext.Provider value={{
-            ...authState,
-            login
+            ...authState, // Pasamos el estado de autenticación al contexto
+            login // Pasamos la función de inicio de sesión al contexto
         }}>
-            {children}
+            {children} {/* Renderizamos los componentes hijos */}
         </AuthContext.Provider>
     )
 }
--- src/auth/context/AuthReducer.js
+++ src/auth/context/AuthReducer.js
@@ -1,19 +1,22 @@
-import { types } from "../types/types";
+import { types } from "../types/types"; // Importamos los tipos de acciones
 
 // Implementa el reducer para gestionar el estado de autenticación
 export const AuthReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.login:
+            // Si la acción es de tipo 'login', actualizamos el estado para indicar que el usuario ha iniciado sesión
             return {
                 ...state,
-                logged: true,
-                user: action.payload  // Establece el nombre del usuario al iniciar sesión
+                logged: true, // Establecemos logged como true para indicar que el usuario ha iniciado sesión
+                user: action.payload  // Establecemos la información del usuario (nombre) en el estado
             };
         case types.logout:
+            // Si la acción es de tipo 'logout', actualizamos el estado para indicar que el usuario ha cerrado sesión
             return {
-                logged: false,  // Establece logged como falso al cerrar sesión
+                logged: false,  // Establecemos logged como false para indicar que el usuario ha cerrado sesión
             };
         default:
+            // Si la acción no coincide con ningún tipo conocido, retornamos el estado sin realizar cambios
             return state;
     }
 }
